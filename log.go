ca4f1148065bf52f41532ecb855e2cbdb8492184
Justin Tan <justin@driver.xyz>
2018-10-16 12:17:32 +0800

    Print microseconds in std log

--- log.go
+++ log.go
@@ -40,14 +40,14 @@ func (l Level) String() string {
 }
 
 const (
-	Ldate         = 1 << iota                              // the date in the local time zone: 2009/01/23
-	Ltime                                                  // the time in the local time zone: 01:23:23
-	Lmicroseconds                                          // microsecond resolution: 01:23:23.123123.  assumes Ltime.
-	Llongfile                                              // full file name and line number: /a/b/c/d.go:23
-	Lshortfile                                             // final file name element and line number: d.go:23. overrides Llongfile
-	LUTC                                                   // if Ldate or Ltime is set, use UTC rather than the local time zone
-	Lfunction                                              // function name and line number: print:23. overrides Llongfile, Lshortfile
-	LstdFlags     = Ldate | Ltime | Lshortfile | Lfunction // initial values for the standard logger
+	Ldate         = 1 << iota                                      // the date in the local time zone: 2009/01/23
+	Ltime                                                          // the time in the local time zone: 01:23:23
+	Lmicroseconds                                                  // microsecond resolution: 01:23:23.123123.  assumes Ltime.
+	Llongfile                                                      // full file name and line number: /a/b/c/d.go:23
+	Lshortfile                                                     // final file name element and line number: d.go:23. overrides Llongfile
+	LUTC                                                           // if Ldate or Ltime is set, use UTC rather than the local time zone
+	Lfunction                                                      // function name and line number: print:23. overrides Llongfile, Lshortfile
+	LstdFlags     = Ldate | Lmicroseconds | Lshortfile | Lfunction // initial values for the standard logger
 )
 
 var globals = struct {
