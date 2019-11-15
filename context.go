17603dec954be10d2c42455ed31d8bf2882d957c
JustinTan <mrnatande@gmail.com>
2019-11-15 10:53:41 +0800

    Deprecated methods

--- context.go
+++ context.go
@@ -4,6 +4,11 @@ import "context"
 
 const keyLogger = "_logger"
 
+// Deprecated: use BuildContext
+func ContextWithLogger(ctx context.Context, l Logger) context.Context {
+	return BuildContext(ctx, l)
+}
+
 func BuildContext(ctx context.Context, l Logger) context.Context {
 	return context.WithValue(ctx, keyLogger, l)
 }
@@ -14,4 +19,9 @@ func FromContext(ctx context.Context) Logger {
 		return l
 	}
 	return defaultLogger
+}
+
+// Deprecated: use FromContext
+func ContextLogger(ctx context.Context) Logger {
+	return FromContext(ctx)
 }
